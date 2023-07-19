8db49251dcb0ad29c4d263bb90966ef8e2209f26
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2023-07-19 16:03:04 +0530

    Update backend.sh

--- Infrastructure_Definition/bin/backend.sh
+++ Infrastructure_Definition/bin/backend.sh
@@ -2,32 +2,31 @@
 DIRNAME=$(git log -1 --name-only --pretty=format:'' | xargs -n1 dirname | sort -u | head -1 | cut -d'/' -f2)
 
 # Checking whether the folder already has a file or not 
-file=$(aws s3 ls s3://stack-definition/$DIRNAME | awk '{print $2}')
+file=$(aws s3 ls s3://stack-definition/$DIRNAME/ | grep "provider.tf")
+code=$?
 
-if [ $file == "provider.tf" ]; then
-# Generating the provider.tf file
-cat > provider.tf << EOF
-# configure aws provider
-provider "aws" {
-  region  = var.region
-  profile = "dhsoni"
-}
+if [ $code != 0 ]; then
+    # Generating the provider.tf file   
+    cat > provider.tf << EOF
+    # configure aws provider
+    provider "aws" {
+      region  = var.region
+      profile = "dhsoni"
+    }
 
-# configure backend
-terraform {
-  backend "s3" {
-    bucket         = "dhsoni-terraform"
-    key            = "$DIRNAME/terraform.tfstate"
-    region         = "us-east-2"
-    profile        = "dhsoni"
-    dynamodb_table = "terraform-state-lock-dynamodb"
-  }
-}
+    # configuring backend
+    terraform {
+    backend "s3" {
+        bucket         = "dhsoni-terraform"
+        key            = "$DIRNAME/terraform.tfstate"
+        region         = "us-east-2"
+        profile        = "dhsoni"
+        dynamodb_table = "terraform-state-lock-dynamodb"
+      }
+    }
 EOF
-
-# Uploading the provider.tf file on S3
-aws s3 cp provider.tf s3://stack-definition/$DIRNAME/provider.tf
-
+    # Uploading the provider.tf file on S3
+    aws s3 cp provider.tf s3://stack-definition/$DIRNAME/provider.tf
 else
     echo "Done" > /dev/null
 fi
