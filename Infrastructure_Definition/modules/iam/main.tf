479270a120422eab518905c8048890a485270a29
Dhruvin Soni <37222087+DhruvinSoni30@users.noreply.github.com>
2023-07-26 15:35:57 +0530

    Update main.tf

--- Infrastructure_Definition/modules/iam/main.tf
+++ Infrastructure_Definition/modules/iam/main.tf
@@ -6,5 +6,5 @@ data "aws_iam_role" "admin_role" {
 # Attach role to instance profile 
 resource "aws_iam_instance_profile" "admin_instance_profile" {
   name = "admin-instance-profile"
-  role = data.aws_iam_role.admin_role
+  role = data.aws_iam_role.admin_role.id
 }
