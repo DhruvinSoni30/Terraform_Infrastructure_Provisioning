26567c412e174e17bae088afdc59906f70cbd370
Dhruvin Soni2 <DhruvinSoni30>
2023-07-28 14:45:40 +0530

    Updating code

--- Master_Definition/main.tf
+++ Master_Definition/main.tf
@@ -14,7 +14,7 @@ module "asg" {
 
 # create security group
 module "security_groups" {
-  source     = "./modules/security-groups"
+  source     = "./modules/security-group"
   vpc_id     = module.vpc.vpc_id
   ssh_access = var.ssh_access
   env        = var.env
